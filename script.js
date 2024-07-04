9d8fbf6ce9fbac2e53d4cfe9e816e93d250beb4d
jessjuniper <154564136+jessjuniper@users.noreply.github.com>
2024-07-04 17:27:09 -0400

    Create operator function for main 4 operations

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ function divide(a,b) {
 let firstVar = 0;
 let secondVar = 0;
 let operator = '';
+
+function operate(firstVar, secondVar, operator) {
+    if (operator == '+') {
+        return add(firstVar, secondVar);
+    }
+    else if (operator == '-') {
+        return subtract(firstVar, secondVar);
+    }
+    else if (operator == '*') {
+        return multiply(firstVar, secondVar);
+    }
+    else if (operator == '/') {
+        return divide(firstVar, secondVar);
+    }
+    else {
+        console.log("Something's wrong!");
+    }
+}
